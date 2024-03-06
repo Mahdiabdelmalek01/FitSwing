33beaaa1f0cb43ac857dfd003a415d65f9b99f6e
Mahdiabdelmalek01 <Mahdi01abdelmalek@gmail.com>
2024-03-06 18:24:38 +0100

    notification_to_activity

--- app/src/main/java/com/example/fitswing/ui/activity/ActivityViewModel.java
+++ app/src/main/java/com/example/fitswing/ui/activity/ActivityViewModel.java
@@ -1,16 +1,16 @@
-package com.example.fitswing.ui.notifications;
+package com.example.fitswing.ui.activity;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class ActivityViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public ActivityViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is activity fragment");
     }
 
     public LiveData<String> getText() {
